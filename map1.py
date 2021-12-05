9318b6d3cd444f066c765c4fdb9dcfbc98b76635
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-12-05 19:23:09 +0100

    feat: add html on popups

--- map1.py
+++ map1.py
@@ -6,6 +6,12 @@ latitude = list(data["LAT"])
 longitude = list(data["LON"])
 name = list(data["NAME"])
 elevation = list(data["ELEV"])
+html = """
+<div class="container" style="font-family:sans-serif;">
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank"><strong>%s</strong></a><br />
+Height: %s m
+</div>
+"""
 
 map = folium.Map(
     location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain"
@@ -14,10 +20,11 @@ fg = folium.FeatureGroup(name="My Map")
 
 
 for lat, lon, ele, nam in zip(latitude, longitude, elevation, name):
+    iframe = folium.IFrame(html=html % (nam, nam, ele), width=200, height=100)
     fg.add_child(
         folium.Marker(
             location=[lat, lon],
-            popup=f"<strong>{nam}</strong>\n{ele} meters",
+            popup=folium.Popup(iframe),
             icon=folium.Icon(color="green"),
         )
     )
